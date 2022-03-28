b71b1e613016b476cc2f721c130d09182798fa32
Ryu jongmoon <0917jong@gmail.com>
2022-03-28 23:58:00 +0900

    boj / gcd

---
+++ BaekjoonOnlineJudge/1188.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n, m; cin >> n >> m;
+    cout << m - __gcd(n, m) << "\n";
+    return 0;
+}
