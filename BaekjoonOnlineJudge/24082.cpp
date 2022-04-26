7dd7e27e14192c5c8b5e4df1d94779c2b49a8cae
Ryu jongmoon <0917jong@gmail.com>
2022-04-27 00:56:39 +0900

    boj / math

---
+++ BaekjoonOnlineJudge/24082.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    ll x; cin >> x;
+    cout << x * x * x << "\n";
+    return 0;
+}
