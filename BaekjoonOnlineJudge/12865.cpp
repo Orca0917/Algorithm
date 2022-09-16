cfe597d0b9a4a90450a7741df661ceee80b4d387
moon <0917jong@gmail.com>
2022-09-17 00:41:41 +0900

    boj / dp, napsack

---
+++ BaekjoonOnlineJudge/12865.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int n, k;
+int memo[101][100'001];
+vector<pii> bag;
+
+int dp(int idx, int available) {
+    if (idx == n || available == 0) return 0;
+
+    int &ret = memo[idx][available];
+    if (ret != -1) return ret;
+
+    ret = dp(idx + 1, available);
+
+    if (bag[idx].first <= available)
+        ret = max(ret, dp(idx + 1, available - bag[idx].first) + bag[idx].second);
+
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n >> k;    
+    bag.resize(n);
+    for (auto &e : bag) cin >> e.first >> e.second;    
+
+    memset(memo, -1, sizeof(memo));
+
+    cout << dp(0, k) << "\n";
+
+    return 0;
+}
