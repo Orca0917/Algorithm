4c4a83876313458409f9f384d89dc5e8ec46272e
moon <0917jong@gmail.com>
2022-11-07 23:59:45 +0900

    (boj) prefix sum

--- BaekjoonOnlineJudge/24525.cpp
+++ BaekjoonOnlineJudge/24525.cpp
@@ -9,6 +9,7 @@ int psum[100'001];
 pii skpsum[100'001];
 
 map<int, int> m;
+string s;
 
 
 int main() {
@@ -16,21 +17,27 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
     
-    string s; cin >> s;
+    memset(psum, 0, sizeof(psum));
+    m.clear();
+    arr.clear();
+
+    cin >> s;
+
     for (char c : s) {
         if (c == 'S') arr.push_back(-2);
         else if (c == 'K') arr.push_back(1);
         else arr.push_back(0);
     }
 
-    // skpsum[0] = {s[0] == 'S', s[0] == 'K'};
+    skpsum[0] = {s[0] == 'S', s[0] == 'K'};
     for (int i = 1; i < s.length(); ++i) {
-        skpsum[i].first = skpsum[i - 1].first + (s[i] == 'S');
-        skpsum[i].second = skpsum[i - 1].second + (s[i] == 'K');
+        skpsum[i].first = skpsum[i - 1].first + (s[i] == 'S' ? 1 : 0);
+        skpsum[i].second = skpsum[i - 1].second + (s[i] == 'K' ? 1 : 0);
     }
 
     psum[0] = arr[0];
     m[psum[0]] = 0;
+    m[0] = -1;
 
     for (int i = 1; i < s.length(); ++i) {
         psum[i] = psum[i - 1] + arr[i];
@@ -40,16 +47,12 @@ int main() {
     int answer = -1;
     for (int i = 2; i < s.length(); ++i) {
         int A = psum[i];
-
-        // cout << A << "\n";
-
         if (m.find(A) != m.end()) {
             int j = m[A];
-            int S = skpsum[i].first - skpsum[j - 1].first;
-            int K = skpsum[i].second - skpsum[j - 1].second;
-
-            if (S > 0 && K > 0)
-                answer = max(answer, i - m[A]);
+            int S = skpsum[i].first - (j == -1 ? 0 : skpsum[j].first);
+            int K = skpsum[i].second - (j == -1 ? 0 : skpsum[j].second);
+            if (S >= 1 && S * 2 == K)
+                answer = max(answer, i - j);
         }
     }
 
