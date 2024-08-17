508bffa101a97f8805cb7be37344f3f410c1af4f
moon <0917jong@gmail.com>
2024-08-17 16:01:24 +0900

    (boj) math

--- BaekjoonOnlineJudge/4994.cpp
+++ BaekjoonOnlineJudge/4994.cpp
@@ -5,19 +5,8 @@
 using namespace std;
 
 int n;
-bool is_prime[201];
 vector<int> primes;
 map<int, ll> m;
-
-// 에라토스테네스의 체로 소수 판별
-void eratos() {
-    memset(is_prime, true, sizeof(is_prime));
-    is_prime[0] = is_prime[1] = false;
-
-    for (int i = 2; i <= 200; ++i)
-        for (int j = 2; i * j <= 200; ++j)
-            is_prime[i * j] = false;
-}
 
 // 0과 1로만 이루어진 수를 만들기 위해 이진법을 적용 (이진법을 숫자 그대로 사용)
 ll int2bitll(int n) {
@@ -31,8 +20,8 @@ ll int2bitll(int n) {
 
 // 소수들만을 가지고 있는 리스트와, 각 소수들은 어떤 이진수로 나누어지는지 확인
 void prepare() {
-    for (int i = 2; i <= 200; ++i)
-        if (is_prime[i])
+    for (int i = 1; i <= 200; ++i)
+        // if (is_prime[i])
             primes.push_back(i);
 
     for (int prime : primes) {
@@ -41,6 +30,7 @@ void prepare() {
             ll value = int2bitll(v++);
             if (value % prime == 0) {
                 m[prime] = value;
+                cout << value << "\n";
                 break;
             }
         }
@@ -55,24 +45,8 @@ int main() {
     // idea : 어떤 양의 정수 n을 인수분해 하고, 인수분해한 값 들이 0과 1로 만 구성된 수들로 나누어진다면!
     // 그 수들을 모두 곱해도 여전히 0과 1로만 구성되어 있을 것!
     // 인수분해한 수 들은 모두 소수이므로, 소수를 판별하는 것도 필요해 보임
-    eratos();
     prepare();
-
-    while (cin >> n && n) {
-        ll answer = 1;
-
-        // n이 어떤 소수들로 이루어져있는지 확인
-        while (n != 1) {
-            for (int prime : primes) {
-                if (n % prime == 0) {
-                    answer *= m[prime];
-                    n /= prime;
-                    break;
-                }
-            }
-        }
-        cout << answer << "\n";
-    }
+    while (cin >> n && n) cout << m[n] << "\n";
     
     return 0;
 }
