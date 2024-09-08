e05ff6383dd6976394b8fbb9f889fb2bd3dc7b0a
moon <0917jong@gmail.com>
2024-09-08 16:49:42 +0900

    (boj) math

---
+++ BaekjoonOnlineJudge/13909.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define pii pair<int, int>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n; cin >> n;
+    cout << (int)sqrt(n);
+    
+    return 0;
+}
